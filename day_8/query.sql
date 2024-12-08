d118d8d35811aea211a53e3afbee27174c4d2703
yosa-odoo <yosa@odoo.com>
2024-12-08 10:52:02 +0100

    day_8

---
+++ day_8/query.sql
@@ -0,0 +1,26 @@
+with recursive subordinates as (
+    select
+        staff_id,
+        staff_name,
+        manager_id,
+        array[staff_id] as path
+    from staff
+    where staff_id = 1
+
+    union all
+
+    select
+        e.staff_id,
+        e.staff_name,
+        e.manager_id,
+        s.path || e.staff_id
+    from staff e
+    inner join subordinates s on e.manager_id = s.staff_id
+)
+select
+    staff_id,
+    staff_name,
+    array_length(path, 1) as level,
+    array_to_string(path, ',') as path
+from subordinates
+order  by level desc;
