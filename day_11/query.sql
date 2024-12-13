a7e941c24fde09280ac04e5dae10e2460c4ccb3f
yosa-odoo <yosa@odoo.com>
2024-12-13 16:52:01 +0100

    day_11

--- day_11/query.sql
+++ day_11/query.sql
@@ -1,22 +1,23 @@
-with data as (
+with ordered as (
     select
-        date,
-        SUM(quantity) filter (where drink_name = 'Eggnog') as eggnog,
-        SUM(quantity) filter (where drink_name = 'Hot Cocoa') as hot_cocoa,
-        SUM(quantity) filter (where drink_name = 'Peppermint Schnapps') as schnapps
-    from drinks
-    group by date
+        *,
+        case
+            when season = 'Spring' then 1
+            when season = 'Summer' then 2
+            when season = 'Fall' then 3
+            when season = 'Winter' then 4
+        end as season_nb
+    from treeharvests
 )
+
 select
-    date
-from data
-where hot_cocoa = '38' and schnapps = '298' and eggnog = '198';
-
-
--- other solution
-SELECT date FROM drinks WHERE drink_name = 'Hot Cocoa' GROUP BY date HAVING sum(quantity) = 38
-INTERSECT
-SELECT date FROM drinks WHERE drink_name = 'Peppermint Schnapps' GROUP BY date HAVING sum(quantity) = 298
-INTERSECT
-SELECT date FROM drinks WHERE drink_name = 'Eggnog' GROUP BY date HAVING sum(quantity) = 198
-;
+    field_name,
+    harvest_year,
+    season,
+    round(avg(trees_harvested) OVER (
+        partition by field_name
+        order by harvest_year, season_nb desc
+        rows between current row and 2 following
+    ), 2) as tree_avg
+from ordered
+order by tree_avg desc nulls last
